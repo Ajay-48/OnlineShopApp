212cbb3e2c5e0a14c2a6e207247b48137aaa9a73
Ajay-48 <165189310+Ajay-48@users.noreply.github.com>
2025-04-03 15:50:26 +0530

    Delete k8-cluster directory

--- k8-cluster/modules/aks/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "kube_config" {
-    description = "The raw kubeconfig for the AKS cluster."
-    value     = azurerm_kubernetes_cluster.aks.kube_config_raw
-    sensitive = true
-}
-output "aks_cluster_name" {
-    description = "The name of the AKS cluster."
-    value = azurerm_kubernetes_cluster.aks.name
-}
