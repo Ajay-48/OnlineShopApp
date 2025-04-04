fccc3b56a1d24be77b989d1daffbeaa3e0d49ce1
Ajay-48 <165189310+Ajay-48@users.noreply.github.com>
2025-04-04 21:28:22 +0530

    Delete infrasetup directory

--- infrasetup/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "terraformBackend-rg"
-    storage_account_name = "tfbackendsc"
-    container_name       = "tfstatefiles"
-    key                  = "aks/terraform.tfstate"
-  }
-}
--- infrasetup/modules/acr/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "acr_name" {
-    description = "The name of the ACR registry."
-    value = azurerm_container_registry.acr.name
-}
